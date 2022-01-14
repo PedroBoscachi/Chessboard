2f02917f1b30f5fbdd9450406907f6b2cccfdc73
PedroBoscachi <pedroboscachi@gmai.com>
2022-01-14 14:26:21 -0300

    Folder Board created, class Position created

---
+++ Xadrez/Board/Entities/Position.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Xadrez.Board.Entities
+{
+    class Position
+    {
+        public int Line { get; set; }
+        public int Column { get; set; }
+
+        public Position()
+        {
+        }
+
+        public Position(int line, int column)
+        {
+            Line = line;
+            Column = column;
+        }
+
+        public override string ToString()
+        {
+            return Line
+                + " - "
+                + Column;
+        }
+    }
+}
